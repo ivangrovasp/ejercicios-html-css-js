b511b960808d51a1b55da3be94cf099243c9f69f
ivangrovasp <165771185+ivangrovasp@users.noreply.github.com>
2024-04-12 13:51:22 +0200

    Delete JS/script.js

--- JS/script.js
+++
@@ -1,5 +0,0 @@
-let botonCmbiarContenidoDIV = document.getElementById("cambiarContenidoDelDiv");
-botonCmbiarContenidoDIV.addEventListener('click', function(){
-    let DivCont = document.getElementById('contenido');
-    DivCont.textContent = 'Hola Mundo'
-});
