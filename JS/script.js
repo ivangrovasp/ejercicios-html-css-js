3961660149111ddeaabd06e5b40af58569bbbfbf
Iván <122996609+Grovass@users.noreply.github.com>
2024-04-12 13:38:31 +0200

    Ejercicio 1 JS completado

---
+++ JS/script.js
@@ -0,0 +1,5 @@
+let botonCmbiarContenidoDIV = document.getElementById("cambiarContenidoDelDiv");
+botonCmbiarContenidoDIV.addEventListener('click', function(){
+    let DivCont = document.getElementById('contenido');
+    DivCont.textContent = 'Hola Mundo'
+});
